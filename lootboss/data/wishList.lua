8377edfb01e2a35d5fc36f4a2b46917828aa10b0
hookstar <lance.hooker@gmail.com>
2021-08-04 10:21:37 -0400

    Add files via upload

---
+++ lootboss/data/wishList.lua
@@ -0,0 +1,775 @@
+return S{
+--Salvage-----------------------
+
+--Bhaflau Remnants
+--(lv15)
+15712,  --Enyo's Leggings
+16097,  --Anu's Tiara
+14559,  --Anu's Doublet
+15630,  --Njord's Trousers
+15728,  --Nemain's Sabots
+14970,  --Hoshikazu Tekko
+
+--Long-Bowed Chariot (lv25)
+16102,  --Bodb's Crown
+14564,  --Bodb's Robe
+14975,  --Ea's Dastanas
+15717,  --Freyr's Ledelsens
+16086,  --Phobos's Mask
+14548,  --Phobos's Cuirass
+15635,  --Tsukikazu Haidate
+
+--(lv35)
+14964,  --Deimos's Gauntlets
+15640,  --Enlil's Brayettes
+14553,  --Freya's Jerkin
+16091,  --Freya's Mask
+14980,  --Macha's Cuffs
+15722,  --Hikazu Sune-Ate
+
+--Sky---------------------------
+
+--Olla Grande
+1425,   --Winterstone
+1441,   --Lib. Abjuration
+1445,   --Freyas Tear
+16956,  --Skofnung
+
+--Zipacna
+1424,   --Gem Of The North
+1442,   --Obl. Abjuration
+17804,  --Ushikirimaru
+
+--Ullikummi
+1423,   --Autumnstone
+1442,   --Obl. Abjuration
+18199,  --Ulfhedinn Axe
+
+--Despot
+1422,   --Gem Of The West
+1442,   --Obl. Abjuration
+18044,  --Scarecrow Scythe
+
+--Mother Globe
+1419,   --Springstone
+1441,   --Lib. Abjuration
+17774,  --Shiranui
+
+--Steam Cleaner
+1418,   --Gem Of The East
+1441,   --Lib. Abjuration
+1445,   --Freyas Tear
+17511,  --Indra Katars
+
+--Faust
+1421,   --Summerstone
+1442,   --Obl. Abjuration
+16838,  --Tonbo-giri
+
+--Brigandish Blade
+1420,   --Gem Of The South
+1441,   --Lib. Abjuration
+
+--Genbu
+12296,  --Genbus Shield
+12434,  --Genbus Kabuto
+1324,   --A.abjuration Hd.
+1326,   --A.abjuration Hn.
+1331,   --M.abjuration Hn.
+1338,   --W.abjuration Ft.
+722,    --Divine Log
+901,    --Venomous Claw
+1404,   --Seal Of Genbu
+3083,   --Mngl. Ck. Skin
+
+--Seiryu
+722,    --Divine Log
+17659,  --Seiryus Sword
+1314,   --D.abjuration Hd.
+1327,   --A.abjuration Lg.
+1329,   --M.abjuration Hd.
+1336,   --W.abjuration Hn.
+903,    --Dragon Talon
+1133,   --Dragon Blood
+1312,   --Angel Skin
+4272,   --Dragon Meat
+4486,   --Dragon Heart
+1405,   --Seal Of Seiryu
+12690,  --Seiryus Kote
+3083,   --Mngl. Ck. Skin
+
+--Byakko
+658,    --Damascus Ingot
+12818,  --Byakkos Haidate
+1317,   --D.abjuration Lg.
+1323,   --E.abjuration Ft.
+1324,   --A.abjuration Hd.
+722,    --Divine Log
+1341,   --N.abjuration Hn.
+860,    --Behemoth Hide
+1406,   --Seal Of Byakko
+18198,  --Byakkos Axe
+3083,   --Mngl. Ck. Skin
+
+--Suzaku
+747,    --Ocl. Ingot
+12946,  --Suzakus Sune-ate
+1316,   --D.abjuration Hn.
+1319,   --E.abjuration Hd.
+1327,   --A.abjuration Lg.
+1343,   --N.abjuration Ft.
+901,    --Venomous Claw
+1313,   --Sirens Hair
+1407,   --Seal Of Suzaku
+18043,  --Suzakus Scythe
+3083,   --Mngl. Ck. Skin
+
+--Kirin
+658,    --Damascus Ingot
+12562,  --Kirins Osode
+17567,  --Kirins Pole
+1315,   --D.abjuration Bd.
+1337,   --W.abjuration Lg.
+1340,   --N.abjuration Bd.
+3083,   --Mngl. Ck. Skin
+
+--Dynamis-----------------------
+
+--Mildaunegeux
+1556,   --Atst. Of Might
+1564,   --Atst. Of Legerity
+1570,   --Atst. Of Accuracy
+
+--Quiebitiel
+1557,   --Atst. Of Celerity
+1566,   --Atst. Of Sacrifice
+1569,   --Atst. Of Harmony
+
+--Goublefaupe
+1558,   --Atst. Of Glory
+1559,   --Atst. Of Right
+1561,   --Atst. Of Force
+1821,   --Atst. Of Invuln.
+
+--Dagourmarche
+1560,   --Atst. Of Bravery
+1563,   --Atst. Of Fortitude
+1567,   --Atst. Of Virtue
+
+--Velosareon
+1562,   --Atst. Of Vigor
+1565,   --Atst. Of Decisiv.
+1568,   --Atst. of Trascen.
+
+--Animated Knuckles
+1571,   --Mystic Fragment
+
+--Animated Dagger
+1572,   --Ornate Fragment
+
+--Animated Longsword
+1573,   --Holy Fragment
+
+--Animated Claymore
+1574,   --Intricate Fragment
+
+--Animated Tabar
+1575,   --Runeaic Fragment
+
+--Aninmated Great Axe
+1576,   --Seraphic Fragment
+
+--Animated Spear
+1578,   --Stellar Fragment
+
+--Animated Scythe
+1577,   --Tenebrous Frag.
+
+--Animated Kunai
+1579,   --Demoniac Frag.
+
+--Animated Tachi
+1580,   --Divine Fragment
+
+--Animated Hammer
+1581,   --Heavenly Frag.
+
+--Animated Staff
+1582,   --Celestial Frag.
+
+--Animated Longbow
+1583,   --Snarled Fragment
+
+--Animated Gun
+1585,   --Etheral Fragment
+
+--Animated Horn
+1584,   --Mysterial Frag.
+
+--Animated Shield
+1822,   --Supernal Fragment
+
+--Dynamis Lord
+13658,  --Shadow Mantle
+14646,  --Shadow Ring
+
+--GuDha Effigy
+
+19107,	--Azoth
+18015,	--Blau Dolch
+17946,  --Maneater
+13690,	--Forager’s Mantle
+11635,  --Alert Ring
+17790,  --Unji
+17788,	--Sairen
+18089,	--Schiltron Spear
+17999,  --Taillefer’s Dagger
+
+--Sea---------------------------
+
+--Ix'Aern (DRK)
+1854,   --Deed of Moder.
+1902,   --Vice of Avarice.
+
+--Ix'Aern (DRG)
+1870,   --Deed of Sensib.
+1903,   --Vice of Aspersion
+
+--Ix'Aern (MNK)
+1851,   --Deed of Placidity
+1901,   --Vice of Antipathy
+
+--Jailer of Prudence
+1849,   --Sixth Virtue
+15510,  --Prudence Torque
+18397,  --Prudence Rod
+
+--Jailer of Hope
+1847,   --Fifth Virtue
+15509,  --Hope Torque
+17595,  --Hope Staff
+
+--Jailer of Justice
+1848,   --Fourth Virtue
+15508,  --Justice Torque
+17710,  --Justice Sword
+
+--Jailer of Faith
+1856,   --Third Virtue
+15512,  --Faith Torque
+18360,  --Faith Baghnakhs
+
+--Jailer of Temperance
+1850,   --First Virtue
+15513,  --Temp. Torque
+17948,  --Temperance Axe
+
+--Jailer of Fortitude
+1853,   --Second Virtue
+15511,  --Fortitude Torque
+18222,  --Fortitude Axe
+
+--Jailer of Love
+1911,   --Aura of Adulation
+1912,   --Aura of Voracity
+15514,  --Love Torque
+18100,  --Love Halberd
+
+-- Absolute Virtue
+1913,   --Sin of Indignation
+1914,   --Sin of Indolence
+1915,   --Sin of Indulgence
+1916,   --Sin of Invidious.
+1917,   --Sin of Insolence
+1918,   --Sin of Infatuation
+1919,   --Sin of Intemper.
+3212,   --Vision Jewel
+
+--Limbus------------------------
+
+--Proto Omega
+1925,   --Omega's Eye
+1926,   --Omega's Heart
+1927,   --Omega's Foreleg
+1928,   --Omega's Hind Leg
+1929,   --Omega's Tail
+
+--Proto Ultima
+1920,   --Ultima's Cerebrum
+1921,   --Ultima's Heart
+1922,   --Ultima's Claw
+1923,   --Ultima's Leg
+1924,   --Ultima's Tail
+
+--NE Apollo
+--SE Apollo
+--NW Apollo
+--SW Apollo
+
+
+
+
+
+
+--ZNM---------------------------
+
+--Vulpangue
+2616,   --Vulpangue's Wing
+16154,  --Karura Hachigane
+17754,  --Sylphid Epee
+
+--Chamrosh
+2617,   --Chamrosh's Beak
+16000,  --Dragoon's Earring
+16240,  --Erato's Cape
+
+--Cheese Hoarder Gigiroon
+2618,   --Gigiroon's Cape
+16340,  --Armadillo Cuisses
+17755,  --Beast Slayer
+
+--Brass Borer
+2626,   --B. Borer's Cocoon
+
+--Claret
+2627,   --Claret Globule
+16274,  --Almah Torque
+18859,  --Kerykeion
+
+--Ob
+2322,   --Attuner
+2323,   --Tact. Processor
+2324,   --Drum Magazine
+2325,   --Equalizer
+2326,   --Target Marker
+2327,   --Mana Channeler
+2328,   --Eraser
+2329,   --Smoke Screen
+2413,   --Coiler
+2414,   --Steam Jacket
+2628,   --Ob's Arm
+3312,   --Percolator
+3313,   --Vivi-valve
+3314,   --Disruptor
+15999,  --Guignol Earring
+
+--Velionis
+2636,   --Velionis's Bone
+15916,  --Corsair's Belt
+18950,  --White Joker
+
+--Lil' Apkallu
+2637,   --Lil' Apkallu's Egg
+11368,  --Hakke Habaki
+11369,  --Numerist Pumps
+
+--Chigre
+2638,   --Chigre
+15827,  --Insect Ring
+15828,  --Blood Ring
+
+--Iriz Ima
+2147,   --Marid Tusk
+2613,   --Iriz Ima's Hide
+11366,  --Avocat Pigaches
+19154,  --Ferrara
+
+--Iriri Samariri
+2615,   --Iriri Samariri's Hat
+15017,  --Toad Mittens
+16339,  --Pzddock Trousers
+
+--Lividroot Amooshah
+2614,   --Amoosh.s Tendril
+11367,  --Nobushi Kyahan
+16153,  --Reikyo Hairpin
+
+--Anantaboga
+2623,   --Anantaboga's Heart
+18448,  --Hacchonenbutsu
+19109,  --Trilling Dagger
+
+--Reacton
+2624,   --Reacton's Ashes
+17316,  --Bomb Arm
+19028,  --Magic Strap
+19211,  --Reacton Arm
+
+--Dextrose
+2625,   --Dextrose's Blubber
+16338,  --Ruby Seraweels
+18127,  --Achille's Spear
+
+--Zareehkl the Jubilant
+2634,   --Zrkl.s Neckpiece
+18949,  --Zareehkl Scythe
+19108,  --Zareehkl Jambiya
+
+--Verdelet
+2635,   --Verdelet's Wing
+16175,  --Muse Tariqah
+16239,  --Solitaire Cape
+
+--Wulgaru
+2633,   --Wulgaru's Head
+16152,  --Hissho Hachimaki
+16174,  --Riot Shield
+
+--Armed Gears
+2610,   --A. Gears Fragment
+11376,  --Aurum Sabatons
+15022,  --Oracle's Gloves
+18126,  --Tomoe
+19035,  --Thunder Grip
+19036,  --Earth Grip
+18020,  --Mercurial Kris
+
+--Gotoh Zha the Redolent
+2611,   --G. Zhas Necklace
+15023,  --Enkidu's Mittens
+16341,  --Aurum Cuisses
+19032,  --Water Grip
+19037,  --Light Grip
+19153,  --Naglering
+13648,  --Gigant Mantle
+
+--Dea
+2612,   --Dea's Horn
+11377,  --Oracle's Pigaches
+16343,  --Enkidu's Subligar
+18125,  --Cletine
+19031,  --Fire Grip
+19038,  --Dark Grip
+
+--Nosferatu
+2620,   --Nosferatu's Claw
+11378,  --Enkidu's Leggings
+15021,  --Aurum Gauntlets
+17960,  --Labrys
+19033,  --Wind Grip
+19036,  --Earth Grip
+16267,  --Ritter Gorget
+
+--Khromasoul Bhurborlor
+2621,   --Bblr.s Vambrace
+15022,  --Oracle's Gloves
+16176,  --Simba Buckler
+16343,  --Enkidu's Subligar
+19034,  --Ice Grip
+19038,  --Dark Grip
+
+--Achamoth
+2622,   --Acham.s Antenna
+11376,  --Aurum Sabatons
+16342,  --Oracle's Braconi
+17753,  --Organics
+19034,  --Ice Grip
+19035,  --Thunder Grip
+17857,  --Animator +1
+
+--Mahjlaef the Paintorn
+2630,   --Mahjlaef's Staff
+15021,  --Aurum Gauntlets
+16177,  --Legion Scutum
+16342,  --Oracle's Braconi
+19031,  --Fire Grip
+19035,  --Thunder Grip
+16269,  --Morgana's Chokers
+
+--Experimental Lamia
+2631,   --Ex. Lamia Armband
+11378,  --Enkidu's Leggings
+16341,  --Aurum Cuisses
+18595,  --Mekki Shakki
+19032,  --Water Grip
+19036,  --Earth Grip
+15911,  --Buccaneer's Belt
+
+--Nuhn
+2632,   --Nuhn's Esca
+11377,  --Oracle's Pigaches
+15023,  --Enkidu's Mittens
+18430,  --Oninohocho
+19033,  --Wind Grip
+19037,  --Light Grip
+15800,  --Omege Ring
+
+--Tinnin
+2158,   --Hydra Fang
+2172,   --Hydra Scale
+2609,   --Tinnin's Fang
+5564,   --Hydra Meat
+11284,  --Enkidu's Harness
+11364,  --Hachiryu Sune-ate
+16156,  --Oracle's Cap
+16157,  --Enkidu's Cap
+18429,  --Shusui
+18593,  --Alkalurops
+15918,  --Witch Sash
+
+--Sarameya
+2168,   --Cerberus Claw
+2169,   --Cerberus Hide
+2619,   --Sarameya's Hide
+5565,   --Cerbeus Meat
+11283,  --Oracle's Robe
+16155,  --Aurum Armet
+16156,  --Oracle's Cap
+16337,  --Hachiryu Haidate
+18446,  --Pachipachio
+18497,  --Foolkiller
+16117,  --Valhalla Helm
+14577,  --Valhalla Brstplate
+
+
+--Tyger
+2371,   --Khimaira Horn
+2372,   --Khimaira Mane
+2373,   --Khimaira Tail
+2629,   --Tyger's Tail
+11282,  --Aurum Cuirass
+15015,  --Hachiryu Kote
+16155,  --Aurum Armet
+16157,  --Enkidu's Cap
+18857,  --Antares
+18948,  --Enforcer
+15657,  --Shadow Trews
+16002,  --Roundel Earring
+
+--Pandemonium Warden
+11631,  --Blobnag Ring
+11634,  --Veela Ring
+11630,  --Corneus Ring
+11281,  --Hachiryu Haramaki
+18594,  --Dorje
+18759,  --Shlng. Baghnakhs
+18447,  --Nanatsusaya
+3228,   --Voyage Jewel
+11628,  --Strigoi Ring
+19051,  --Vivid Strap +1
+11633,  --Galdr Ring
+11629,  --Zilant Ring
+11632,  --Karka Ring
+11502,  --Acubus Helm
+949, 	--Rain Lily
+
+--HNM---------------------------
+
+--Fafnir
+867,    --Dragon Scales
+13914,  --Aegishjalmr
+14075,  --Andvaranauts
+16555,  --Ridill
+16942,  --Balmung
+17653,  --Hrotti
+1321,   --E.abjuration Hn.
+1326,   --A.abjuration Hn.
+1328,   --A.abjuration Ft.
+1339,   --N.abjuration Hd.
+903,    --Dragon Talon
+1133,   --Dragon Blood
+4272,   --Dragon Meat
+4486,   --Dragon Heart
+3340,   --Sweet Tea
+865,    --Nidhogg Scales
+
+--Nidhogg
+865,    --Nidhogg Scales
+1526,   --Wyrm Beard
+1320,   --E.abjuration Bd.
+1325,   --A.abjuration Bd.
+1330,   --M.abjuration Bd.
+1342,   --N.abjuration Lg.
+867,    --Dragon Scales
+903,    --Dragon Talon
+1133,   --Dragon Blood
+4272,   --Dragon Meat
+4486,   --Dragon Heart
+
+--Behemoth
+860,    --Behemoth Hide
+3342,   --Savory Shank
+16869,  --Thundercloud
+17294,  --Comet Tail
+
+--King Behemoth
+831,    --Shining Cloth
+13415,  --Pixie Earring
+13566,  --Defending Ring
+1527,   --Behemoth Tongue
+1322,   --E. Abjuration Lg.
+1328,   --A. Abjuration Ft.
+1332,   --M. Abjuration Lg.
+860,    --Behemoth Hide
+883,    --Behemoth Horn
+1334,   --W. Abjuration Hd.
+
+--Adamantoise
+13794,  --Heavy Cuirass
+12361,  --Sipar
+3344,   --Red Pondweed
+
+--Aspidochelone
+13794,  --Heavy Cuirass
+1525,   --Adamantoise Egg
+12361,  --Sipar
+1318,   --D. Ajuration Ft.
+1325,   --A. Abjuration Bd.
+1333,   --M. Abjuration Ft.
+1335,   --W. Abjuration Bd.
+1335,   --W. Abjuration Bd.
+
+--Tiamat
+14869,  --Noritsune Kote
+15322,  --Herald's Gaiters
+658,    --Damascus Ingot
+722,    --Divine Log
+860,    --Behemoth Hide
+903,    --Dragon Talon
+1133,   --Dragon Blood
+1313,   --Siren's Hair
+1816,   --Wyrm Horn
+4272,   --Dragon Meat
+4486,   --Dragon Heart
+
+--Shikigami Weapon
+14468,  --Yinyang Robe
+
+--King Arthro
+15899,  --Velocious Belt
+
+--Voluptuous Vivian
+13301,  --Vivian Ring
+13574,  --Black Ribbon
+
+--Bomb Queen 
+13567,	--Bomb Queen Ring
+16426,	--Avengers
+
+--Hakutaku
+13915,	--Optical Hat
+
+--ToAU HNM----------------------
+
+--Khimaira
+2371,   --Khimaira Horn
+2372,   --Khimaira Mane x2
+2373,   --Khimaira Tail
+17738,  --Hauteclaire
+18847,  --Seveneyes
+14777,	--Summoning Earring
+14772,	--Dark Earring
+14774,	--Enfeebling Earring
+14780,	--String Earring
+14769,	--Buckler Earring
+14771,	--Divine Earring
+14779,	--Singing Earring
+14768,	--Parrying Earring
+14767,	--Evasion Earring
+14776,	--Healing Earring
+14778,	--Ninjutsu Earring
+14775,	--Elemental Earring
+14781,	--Wind Earring
+14770,	--Guarding Earring
+14773,	--Augmenting Earring
+
+--Cerberus
+2168,   --Cerberus Claw
+2169,   --Cerberus Hide
+5565,   --Cerberus Meat
+18385,  --Algol
+
+--Medusa
+2357,   --Medusa's Armlet
+14959,	--Mercenary’s Dastanas
+15971,	--Antivenom Earring
+15974,	--Velocity Earring
+15523,	--Chivalrous Chain
+15525,	--Grandiose Chain
+15884,	--Potent Belt
+15886,	--Precise Belt
+15292,	--Penitent’s Rope
+15490,	--Miraculous Cape
+15492,	--Intensifying Cape
+15494,	--Invigorating Cape
+--WotG HNM----------------------
+
+--Dark Ixion
+4306,   --Giant Donko
+19107,  --Azoth
+19041,  --Rose Strap
+18734,  --Sturms Report
+11289,  --Ixion Cloak
+16242,  --Ixion Cape
+2654,   --Dark Ixion Horn
+2655,   --Dark Ixion Tail
+
+--Serket (Sandworm)
+901,    --Venomous Claw
+11287,  --Antares Harness
+12348,  --Serket Shield
+13552,  --Serket Ring
+16767,  --Triple Dagger
+19213,  --White Tathlum
+
+--Guivre (Sandworm)
+909,    --Guivre's Skull
+11288,  --Zahak's Mail
+19212,  --Black Tathlum
+
+--King Arthro (Sandworm)
+836,    --Damascene Cloth
+11286,  --Avalon Breastplate
+15899,  --Velocious Belt
+16178,  --Avalon Shield
+
+--Lambton Worm (Sandworm)
+901,    --Venomous Claw
+836,    --Damascene Cloth
+909,    --Guivre's Skull
+11285,  --Mrgn. Cotehardie
+16344,  --Oily Trousers
+17751,  --Fragarach 
+16275,  --Anicent Torque
+
+--Mythic NM---------------------
+18969,  --Terpsichore
+18970,  --Tupsimati
+18971,  --Conqueror
+18972,  --Glanzfaust
+18973,  --Yagrush
+18974,  --Laevateinn
+18975,  --Murgleis
+18976,  --Vajra
+18977,  --Burtgang
+18978,  --Liberator
+18979,  --Aymur
+18980,  --Carnwenhan
+18981,  --Gastraphetes
+18982,  --Kogarasumaru
+18983,  --Nagi
+18984,  --Ryunohige
+18985,  --Nirvana
+18986,  --Tizona
+18987,  --Death Penalty
+18988,  --Kenkonken
+
+--BCNM -------------------------
+2108,   --Monarchs Orb
+
+--SN Custom---------------------
+
+1872, -- Ghrah M Chip
+
+--Random NM --------------------
+14873, --Bandomusha Kote
+18002, --Perseuss Harpe
+15182, --Zoolater Hat
+
+--TEST--------------------------
+14443, -- Vampire Cloak
+15338, -- Vampire Boots
+14783, -- Vampire Earring
+15197, -- Vampire Mask
+16296, -- Armiger's Lace
+}
